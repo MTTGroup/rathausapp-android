f379f38fd6badc1df4de5b2aefc568d2eefe69d8
vpham <phamanhviet@gmail.com>
2014-04-07 01:19:30 +0200

    add parse sdk
    adding parse sdk configuration and libs

--- rathausapp/src/de/mtt/rathaus/android/RathausApplication.java
+++ rathausapp/src/de/mtt/rathaus/android/RathausApplication.java
@@ -1,6 +1,34 @@
 package de.mtt.rathaus.android;
 
+import android.app.Application;
 
-public class RathausApplication {
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+
+public class RathausApplication extends Application {
+
+	public static final String PARSE_APPLICATION_ID = "LChRD35GzrvEBx0wous91BOz3H21EavBgg6iDOur";
+
+	public static final String PARSE_CLIENT_KEY = "YwlxevjJBBh2BM0FOoK3BAIamrQc54vRWnbtcjsj";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Add your initialization code here
+		Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
+
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+
+		// If you would like all objects to be private by default, remove this line.
+		defaultACL.setPublicReadAccess(true);
+
+		ParseACL.setDefaultACL(defaultACL, true);
+
+	}
 
 }
