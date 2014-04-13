6606726e497ddd5e38cedd45124c2ac85c137589
vpham <phamanhviet@gmail.com>
2014-04-13 02:14:38 +0200

    prepare other menu fragments
    prepare all other menu fragments

--- rathausapp/src/de/mtt/rathaus/android/fragments/AboutFragment.java
+++ rathausapp/src/de/mtt/rathaus/android/fragments/AboutFragment.java
@@ -46,7 +46,7 @@ public class AboutFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_events, container, false);
 		return rootView;
 	}
 
--- rathausapp/src/de/mtt/rathaus/android/fragments/JobsFragment.java
+++ rathausapp/src/de/mtt/rathaus/android/fragments/JobsFragment.java
@@ -42,7 +42,7 @@ public class JobsFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_events, container, false);
 		return rootView;
 	}
 
